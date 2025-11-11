88a63925d86f7a96cdc5fc028d8aa30fa217215a
Giovanna Migliorini <giovanna.migliorini@eaportal.org>
2025-11-11 16:02:07 -0300

    Commit teste

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import express, {request, response} from "express"
+import cors from "cors"
+import mysql2 from "mysql2"
+import { persons } from "./persons.js"
+
+const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env
+
+const app = express()
+const port = 3333
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (request, response) => {
+    response.json(persons)
+})
+
+app.post("/cadastrar", (request, response) => {
+    const { user } = request.body;
+    console.log(user);
+
+ const insertCommand = `
+       INSERT INTO giovannamigliorini_02mb (name, email, password)
+       VALUES(?, ?, ?)
+    `
+
+    database.query(insertCommand, [user.name, user.email, user.password], (error) => {
+        if(error) {
+            console.log(error)
+            return
+        }
+
+          response.status(201).json({ message: "Usúario cadastrado com sucesso!" })
+    })                                                                        
+
+      
+    })
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}!`)
+});
+
+const database = mysql2.createPool({
+    host: DB_HOST,
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    connectionLimit: 10
+})
